24c7fefb1d9cb0ca194e0f5db39548e20b83d53c
Julius Dejon <dejonjulz@gmail.com>
2018-04-21 22:52:41 +0800

    Added Portfolio Projects

--- src/components/SectionB.js
+++ src/components/SectionB.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
-import me from '../images/me.png';
+import csta from '../images/csta.png';
+import piggyback from '../images/piggyback-logo.png';
+import mnemosyne from '../images/mnemosyne.png';
+import {Button} from 'antd';
 class SectionB extends Component {
   render() {
     return (
     <div id="section-b">
       <h2>Projects</h2>
     <section className="SectionB">
-      <div className="cards">
-        <h3> Lorem </h3>
-        <p>This project is to</p>
-      </div>
-      <div className="cards">
-        <h3> Lorem </h3>
-        <p>This project is to</p>
+      <div className="card">
+        <img src={csta} />
+        <div className="card-container">
+        <h3> CSTA Web-Based Library System </h3>
+        <p>Master</p>
+        <Button type="primary">Visit</Button>
         </div>
-      <div className="cards">
-        <h3> Lorem </h3>
-        <p>This project is to</p>
+      </div>
+      <div className="card">
+        <img src={piggyback} className="imgResize"/>
+        <div className="card-container">
+        <h3> Piggyback </h3>
+        <p>Contributor</p>
+        <Button type="primary">Visit</Button>
+        </div>
+        </div>
+      <div className="card">
+      <img src={mnemosyne} className="imgResize" />
+      <div className="card-container">
+        <h3> Mnemosyne </h3>
+        <p>Contributor</p>
+        <Button type="primary">Visit</Button>
+        </div>
         </div>
     </section>
     </div>
