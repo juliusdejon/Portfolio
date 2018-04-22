54a760770b46845c819821a0f68953be62aa9658
Julius Dejon <dejonjulz@gmail.com>
2018-04-22 16:07:12 +0800

    updated title and logo

--- src/components/showcase.js
+++ src/components/showcase.js
@@ -17,7 +17,6 @@ class ShowCase extends Component {
         <h1>Hello, I'm {title}</h1>
         <h2><Tag color="#2db7f5">Full Stack Web Developer</Tag></h2>
         <p>this portfolio is powered by React <img src={icon} alt="react-icon" style={{width: '3rem'}}/> and antd  <img src={iconAntd} alt="antd-icon" style={{width: '2rem'}}/></p>
-        <br /><br /><br /> <br />
           <Button ghost size="large"><a href="#AboutMe">Read More!</a></Button> 
       </div>
     </div>
