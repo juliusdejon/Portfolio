98b417be58c730800b2d9c617c724a8f325e43f0
Julius Dejon <dejonjulz@gmail.com>
2018-04-22 16:08:45 +0800

    updated tag color so it doesnt look like a link

--- src/components/showcase.js
+++ src/components/showcase.js
@@ -15,7 +15,7 @@ class ShowCase extends Component {
     <div>
       <div className="showcase-container">
         <h1>Hello, I'm {title}</h1>
-        <h2><Tag color="#2db7f5">Full Stack Web Developer</Tag></h2>
+        <h2><Tag color="#1abc9c">Full Stack Web Developer</Tag></h2>
         <p>this portfolio is powered by React <img src={icon} alt="react-icon" style={{width: '3rem'}}/> and antd  <img src={iconAntd} alt="antd-icon" style={{width: '2rem'}}/></p>
           <Button ghost size="large"><a href="#AboutMe">Read More!</a></Button> 
       </div>
