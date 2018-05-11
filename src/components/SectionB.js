ffe9390d8af96ad1a11c5cc59239bb2ee59b50ee
Julius Dejon <dejonjulz@gmail.com>
2018-05-11 11:27:23 +0800

    added target attribute _blank to links and update Section B

--- src/components/SectionB.js
+++ src/components/SectionB.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import csta from '../images/csta.png';
 import piggyback from '../images/piggyback-logo.png';
 import mnemosyne from '../images/mnemosyne.png';
-import {Button, notification, Icon} from 'antd';
+import { Button, notification, Icon } from 'antd';
 class SectionB extends Component {
   render() {
     const openNotification = () => {
@@ -13,35 +13,35 @@ class SectionB extends Component {
       });
     };
     return (
-    <div id="section-b">
-      <h2>Projects</h2>
-    <section className="SectionB">
-      <div className="card">
-        <img src={csta} alt="logo" />
-        <div className="card-container">
-        <h3> CSTA Web-Based Library System </h3>
-        <p>Master</p>
-        <Button type="primary" onClick={openNotification}>Visit</Button>
-        </div>
+      <div id="section-b">
+        <h2>Projects</h2>
+        <section className="SectionB">
+          <div className="card">
+            <img src={csta} alt="logo" />
+            <div className="card-container">
+              <h3> CSTA Web-Based Library System </h3>
+              <p>Master</p>
+              <Button type="primary" href="http://cstaproject.x10host.com/" target="_blank">Visit</Button>
+            </div>
+          </div>
+          <div className="card">
+            <img src={piggyback} className="imgResize" alt="logo" />
+            <div className="card-container">
+              <h3> Piggyback </h3>
+              <p>Contributor</p>
+              <Button type="primary" href="http://piggyback.primephilippines.com" target="_blank">Visit</Button>
+            </div>
+          </div>
+          <div className="card">
+            <img src={mnemosyne} className="imgResize" alt="logo" />
+            <div className="card-container">
+              <h3> Mnemosyne </h3>
+              <p>Contributor</p>
+              <Button type="primary" onClick={openNotification}>Visit</Button>
+            </div>
+          </div>
+        </section>
       </div>
-      <div className="card">
-        <img src={piggyback} className="imgResize" alt="logo" />
-        <div className="card-container">
-        <h3> Piggyback </h3>
-        <p>Contributor</p>
-        <Button type="primary" href="http://piggyback.primephilippines.com" target="_blank">Visit</Button>
-        </div>
-        </div>
-      <div className="card">
-      <img src={mnemosyne} className="imgResize" alt="logo"  />
-      <div className="card-container">
-        <h3> Mnemosyne </h3>
-        <p>Contributor</p>
-        <Button type="primary" onClick={openNotification}>Visit</Button>
-        </div>
-        </div>
-    </section>
-    </div>
     );
   }
 }
