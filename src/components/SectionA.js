0aa0aa0d383b02963de39940e8a2613469303c6a
Julius Dejon <zjmdejon@globe.com.ph>
2020-04-26 16:22:57 +0800

    Updated abut me

--- src/components/SectionA.js
+++ src/components/SectionA.js
@@ -8,27 +8,28 @@ class SectionA extends Component {
           <h2> About Me </h2>
           <p>
             &emsp;I graduated at the University of the East Majoring Information
-            Technology. In school I learned some programming languages such as
-            C, C++, C#, Java, .NET, HTML, CSS, Python, and PHP. I also learned
-            frameworks such as Bootstrap and JQuery to develop a Web-based
-            system using PHP.
+            Technology. In school I learned some programming languages such as C
+            Java, .NET, Python, and PHP. I also applied Bootstrap and JQuery to
+            develop a Web-based system.
           </p>
           <p>
             &emsp;At my 4 month of <b>Internship</b> I have been exposed to a
             industrial level of Web development. With my co-interns we developed
-            system using ReactJS, antd, Laravel and other related tech.
+            system using ReactJS (react-boilerplate), Antd, Laravel and other
+            related tech.
           </p>
           <p>
             &emsp;After that I worked on a startup company for about 1.4 years,
             We Implement best practices and uses cutting edge technologies, I
             learn alot of things in <b>web development</b> as we thrive to be
-            the best. Technology used (Docker, Elixir, React, Ruby, Terraform,
-            Kubernetes, Vagrant, Ansible, Nginx, AWS, GCP, DO, Postgres,
-            MongoDB, Redis){" "}
+            solve bigger problems. Technology used (Docker, Elixir, React, Ruby,
+            Terraform, Kubernetes, Vagrant, Ansible, Nginx, AWS, GCP, DO,
+            Postgres, MongoDB, Redis), Others: (DDD, TDD, Functional
+            Programming, CQRS + ES).
           </p>
           <p>
-            &emsp;I currently work at a outsourcing company partnered with AWS,
-            I work their as a <b>Software Engineer</b>
+            &emsp;I'm currently working at a outsourcing company partnered with
+            AWS, I work there as a <b>Software Engineer</b>
           </p>
           <p>
             &emsp;Since we strictly have tests in our API's at my previous
