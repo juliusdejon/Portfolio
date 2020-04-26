bcaf182e351b2fba80d6f39a62615ade504b708c
Julius Dejon <zjmdejon@globe.com.ph>
2020-04-26 11:26:21 +0800

    Revisions of About me

--- src/components/SectionA.js
+++ src/components/SectionA.js
@@ -14,33 +14,34 @@ class SectionA extends Component {
             system using PHP.
           </p>
           <p>
-            &emsp;At my 4 month of Internship I have been exposed to a
+            &emsp;At my 4 month of <b>Internship</b> I have been exposed to a
             industrial level of Web development. With my co-interns we developed
             system using ReactJS, antd, Laravel and other related tech.
           </p>
           <p>
             &emsp;After that I worked on a startup company for about 1.4 years,
             We Implement best practices and uses cutting edge technologies, I
-            learn alot of things in web development as we thrive to be the best.
-            Technology used (Docker, Elixir, React, Ruby, Terraform, Kubernetes,
-            Vagrant, Ansible, Nginx, AWS, GCP, DO, Postgres, MongoDB, Redis){" "}
+            learn alot of things in <b>web development</b> as we thrive to be
+            the best. Technology used (Docker, Elixir, React, Ruby, Terraform,
+            Kubernetes, Vagrant, Ansible, Nginx, AWS, GCP, DO, Postgres,
+            MongoDB, Redis){" "}
           </p>
           <p>
-            &emsp;I currently work on a outsourcing company partnered with AWS,
-            I work their as a Software Engineer
+            &emsp;I currently work at a outsourcing company partnered with AWS,
+            I work their as a <b>Software Engineer</b>
           </p>
           <p>
             &emsp;Since we strictly have tests in our API's at my previous
-            company I decided to focus more on testing libraries used are Jest,
-            react-testing-library, mocha, chai, cypress.
+            company I decided to focus more on <b>testing</b> libraries used are
+            Jest, react-testing-library, mocha, chai, cypress.
           </p>
           <p>
-            I'm also a certified AWS cloud practiioner and aiming to be a AWS
-            Solutions Architect
+            I'm also a certified <b>AWS cloud practioner</b> and aiming the
+            solutions architect path.
           </p>
           <p>
-            &emsp;I spend time learning new things via reading documentations
-            and watching tutorials
+            I spend time learning new things via reading documentations and
+            watching tutorials
           </p>
         </div>
         <div className="img-container">
