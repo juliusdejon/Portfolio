01bab88761bf1f28985b31299652562d32000d91
Julius Dejon <zjmdejon@globe.com.ph>
2020-04-26 12:10:29 +0800

    Added Projects

--- src/components/SectionB.js
+++ src/components/SectionB.js
@@ -1,66 +1,98 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import csta from "../images/csta.png";
+import valogo from "../images/valogo.png";
 import piggyback from "../images/piggyback-logo.png";
 import petify from "../images/petify.png";
-import { Button, notification, Icon } from "antd";
-class SectionB extends Component {
-  render() {
-    const openNotification = () => {
-      notification.open({
-        type: "error",
-        message: "Ooops",
-        description:
-          "Kindly visit http://github.com/juliusdejon or Contact me for this matter.",
-        icon: <Icon type="warning" style={{ color: "#303030" }} />
-      });
-    };
-    return (
-      <div id="section-b">
-        <h2>Projects</h2>
-        <section className="SectionB">
-          <div className="card">
-            <img src={csta} alt="logo" />
-            <div className="card-container">
-              <h3> CSTA Web-Based Library System </h3>
-              <p>Master</p>
-              <Button type="primary" onClick={openNotification} target="_blank">
-                Visit
-              </Button>
-            </div>
+import shore from "../images/shore.png";
+import { Button, notification, Icon, Modal } from "antd";
+
+const SectionB = () => {
+  const [modal, setModal] = useState(false);
+  // const openNotification = () => {
+  //   notification.open({
+  //     type: "error",
+  //     message: "Ooops",
+  //     description:
+  //       "Kindly visit http://github.com/juliusdejon or Contact me for this matter.",
+  //     icon: <Icon type="warning" style={{ color: "#303030" }} />,
+  //   });
+  // };
+  return (
+    <div id="section-b">
+      <h2>Projects</h2>
+      <section className="SectionB">
+        <div className="card">
+          <img src={csta} style={{ marginTop: "12px" }} alt="logo" />
+          <div className="card-container">
+            <h3> CSTA Web-Based Library System </h3>
+            <p>Master</p>
+            <Button
+              type="primary"
+              onClick={() => setModal(true)}
+              target="_blank"
+            >
+              View
+            </Button>
           </div>
-          <div className="card">
-            <img src={piggyback} className="imgResize" alt="logo" />
-            <div className="card-container">
-              <h3> Piggyback </h3>
-              <p>Contributor</p>
-              <Button type="primary" onClick={openNotification} target="_blank">
-                Visit
-              </Button>
-            </div>
+        </div>
+        <div className="card">
+          <img src={valogo} alt="logo" />
+          <div className="card-container">
+            <h3> Villa Alfredo's Resort Reservation System </h3>
+            <p>Master</p>
           </div>
-          <div className="card">
-            <img
-              src={petify}
-              className="imgResize"
-              alt="logo"
-              style={{ borderRadius: "50%" }}
-            />
-            <div className="card-container">
-              <h3> Petify </h3>
-              <p>Master</p>
-              <Button
-                type="primary"
-                href="https://petified.herokuapp.com"
-                target="_blank"
-              >
-                Visit
-              </Button>
-            </div>
+        </div>
+
+        <div className="card">
+          <img src={piggyback} className="imgResize" alt="logo" />
+          <div className="card-container">
+            <h3> Piggyback </h3>
+            <p>Contributor</p>
           </div>
-        </section>
-      </div>
-    );
-  }
-}
+        </div>
+        <div className="card">
+          <img
+            src={shore}
+            className="imgResize"
+            style={{ marginTop: "12px" }}
+            alt="logo"
+          />
+          <div className="card-container">
+            <h3> Shore Suite </h3>
+            <p>Contributor</p>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            src={petify}
+            style={{ marginTop: "12px", borderRadius: "16px" }}
+            alt="logo"
+          />
+          <div className="card-container">
+            <h3> Petify </h3>
+            <p>Master</p>
+            <Button
+              type="primary"
+              href="https://petified.herokuapp.com"
+              target="_blank"
+            >
+              Visit
+            </Button>
+          </div>
+        </div>
+        <Modal
+          title="Basic Modal"
+          visible={modal}
+          footer={false}
+          onCancel={() => setModal(false)}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      </section>
+    </div>
+  );
+};
 
 export default SectionB;
